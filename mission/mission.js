b0c572b5abe39b35f438067dffdc7187b3cc6c7c
dylankohn <dylankohn1@gmail.com>
2024-09-28 15:14:59 -0600

    Added an invert option to the site for mission statement 2

---
+++ mission/mission.js
@@ -0,0 +1,18 @@
+const imageMarkup = '<img src="byui-logo_blue.webp" class="byuilogo">';
+document.getElementById('toggleButton').addEventListener('click', function() {
+    document.body.classList.toggle('inverted');
+    const image = document.querySelector('.byuilogo'); // Get the image by its class
+        if (image) {
+            image.remove();
+        } else {
+            document.body.insertAdjacentHTML('beforeend', imageMarkup);
+        }
+});
+
+
+const newImage = document.createElement("img");
+newImage.setAttribute("src", "byui-logo_white.png");
+newImage.setAttribute("alt", "invertedByui");
+newImage.classList.add("inverted-logo"); 
+document.body.appendChild(newImage);
+
